a7cffa6043cdff7e32585f9b51dbe1c309b45779
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-18 09:53:47 -0300

    projeto

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,7 @@
-const authRouter = require("./routes/authRoutes");
 const router = require("express").Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const User = require("../models/user");
+const User = require("../models/users");
 // registrando um usuaário
 router.post("/register", async(req, res)=>{
 const name = req.body.name;
@@ -10,8 +9,63 @@ const email = req.body.email;
 const password = req.body.password;
 const confirmpassword = req.body.confirmpassword;
 //checando se todos os dados foram enviados
-if (name==null || email ==null || password == null || confirmpassword == null){
+if (name == null || email ==null || password == null || confirmpassword == null){
 return res.status(400).json({error : "Por favor, preencha todos os campos"});
 }
+//testando se as senhas são iguais
+if(password != confirmpassword){
+return res.status(400).json({error : "As senhas não conferem!!!"})
+}
+//conferindo se o usuário já existe
+const emailExists = await User.findOne({email : email});
+if(emailExists){
+return res.status(400).json({error : "O e-mail informado já existe."})
+}
+//criando a senha com bcrypt
+const salt = await bcrypt.genSalt(12);
+const passwordHash = await bcrypt.hash(password, salt);
+//criando o usuário após as validações no sistema
+const user = new User({
+name : name,
+email : email,
+password: passwordHash
 });
+//montando um try catch para pega outros erros e afins
+try {
+const newUser = await user.save();
+//criando o token do usuario
+const token = jwt.sign(
+//payload
+{
+name : newUser.name,
+id : newUser._id
+},
+"segredo" //isso tonar o nosso token único
+);
+//retornar o token para o projeto e manda mensagem
+res.json({error: null, msg: "Você fez o cadastro com sucesso!!!", 
+token: token, 
+userId:
+newUser._id});
+} catch(error){
+res.status(400).json({error});
+}
+});
+
+
+//criando a rota de login
+router.post("/login", async(req, res)=>{
+    const email = req.body.email;
+    const password = req.body.password;
+    //e se usuário ja existe?
+    const user = await User.findOne({email : email});
+    if(!user){
+    return res.status(400).json({error : "E-mail não cadastrado, usuário não existe!!!"})
+    }
+    });
+    const checkpassword = await bcrypt.compare(password,user.password);
+if (!checkpassword){
+return res.status(400).json({error: 'Senha inválida'});
+}
+    
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,40 @@
-//modules
-const express = require("express");
-const mongoose = require("mongoose");
-const bodyParser = require("body-parser");
-const cors = require("cors");
+//modulos
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
 //routes
-const authRouter = require("./routes/authRoutes");
-//middlewares - travas do meio de projeto e acesso
+const authRouter = require('./routes/authRoutes');
+//midllewares
+
 //config
 const dbName = "partytime";
 const port = 3000;
 const app = express();
+
+
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-//atrelar as rotas ao express
-app.use("/api/auth", authRouter);
-//conexão mongodb
-mongoose.connect(
-`mongodb://127.0.0.1:27017/${dbName}`, {
-useNewUrlParser : true,
-UseUnifiedTopology : true,
-serverSelectionTimeoutMS : 10000
-}
-)
-app.get("/", (req, res)=> {
-//primeira rota de teste
-res.json({message : "Rota de teste será trocada!!!"});
-});
-//escutando a porta
+
+//atrelando a primeira rota
+app.use("/api/auth", authRouter)
+
+
+//conexao mongodb
+mongoose.connect(`mongodb://127.0.0.1:27017/${dbName}`, {
+    useNewUrlParser:true,
+    useUnifiedTopology : true,
+    serverSelectionTimeoutMS : 20000
+})
+
+
+app.get("/", (req, res)=>{
+    res.json({message : "Rota de teste, será trocada"});
+})
+
+//escutar ou entende a porta 3000
 app.listen(port, ()=>{
-console.log(`O backend está rodando na porta ${port}`)
-});
+    console.log(`O backend está rodando na porta ${port}`)
+})
